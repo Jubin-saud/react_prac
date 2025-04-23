7ce32ccf256468aa259731eb6b60191e6ba0cb2e
zubeen <zubeensaud@gmail.com>
2025-04-23 15:16:17 +0530

    themr

---
+++ 09themeswitcher/src/context/theme.js
@@ -0,0 +1,14 @@
+import { createContext,useContext } from "react";
+
+export const ThemeContext = createContext({
+  thememode: "light",
+  darktheme: () => {},
+  lighttheme: () => {},
+});
+
+export const ThemeProvider = ThemeContext.Provider;
+
+
+export const useTheme = () => {
+  return useContext(ThemeContext);
+}
